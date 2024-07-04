07290aad103c37e6954e3b15ba7cb1622d95aad9
mxnc <thibaultmaxence44@gmail.com>
2024-07-04 08:18:07 +0200

    final fix for assert

--- 0x00-hello-dart/5-assertion.dart
+++ 0x00-hello-dart/5-assertion.dart
@@ -1,9 +1,7 @@
 void main(List<String> args) {
- try {
-    int score = int.parse(args[0]);
-    assert(score >= 80, 'The score must be bigger or equal to 80');
-    print('You passed');
-  } catch (e) {
-    print('Uncaught Error: Assertion failed: "The score must be bigger or equal to 80"');
-  }
+  int nb = int.parse(args[0]);
+  String str = "You Passed";
+  
+  assert(nb >= 80, 'Uncaught Error: Assertion failed: "The score must be bigger or equal to 80');
+  print(str);
 }
