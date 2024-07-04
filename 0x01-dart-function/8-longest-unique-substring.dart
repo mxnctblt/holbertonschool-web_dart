9271b18ea7f751af06f2174d8e0fdfb65a9ced1d
mxnc <thibaultmaxence44@gmail.com>
2024-07-04 14:25:37 +0200

    task 8. Longest Unique Substring

--- 0x01-dart-function/8-longest-unique-substring.dart
+++ 0x01-dart-function/8-longest-unique-substring.dart
@@ -1,5 +1,25 @@
 String longestUniqueSubstring(String str) {
-  print(longestUniqueSubstring("abcacbd"));
-  print(longestUniqueSubstring("aaaaaaaa"));
-  print(longestUniqueSubstring("abcde"));
+  Map<String, int> charsMap = Map<String, int>();
+  int cursor = 0;
+  int startIndex = 0;
+  int maxLength = 0;
+
+  for (int i = 0; i < str.length; i++) {
+    String currentChar = str[i];
+    bool isExistingCharInMap =
+        charsMap[currentChar] != null && charsMap[currentChar]! >= cursor;
+
+    if (isExistingCharInMap) {
+      cursor = charsMap[currentChar]! + 1;
+    }
+
+    charsMap[currentChar] = i;
+    bool isLongerNewString = maxLength < i - cursor + 1;
+
+    if (isLongerNewString) {
+      maxLength = i - cursor + 1;
+      startIndex = cursor;
+    }
+  }
+  return str.substring(startIndex, startIndex + maxLength);
 }
