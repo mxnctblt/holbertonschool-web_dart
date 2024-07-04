c81e3dadcec3dab969996a2d33d54c245a6a45b2
mxnc <thibaultmaxence44@gmail.com>
2024-07-04 08:15:00 +0200

    removing if and using assert

--- 0x00-hello-dart/5-assertion.dart
+++ 0x00-hello-dart/5-assertion.dart
@@ -1,18 +1,9 @@
 void main(List<String> args) {
-  if (args.isEmpty) {
-    print('Uncaught Error: Assertion failed: "Please provide a score as an argument."');
-    return;
-  }
-
-  try {
+ try {
     int score = int.parse(args[0]);
-
-    if (score >= 80) {
-      print('You passed');
-    } else {
-      print('Uncaught Error: Assertion failed: "The score must be bigger or equal to 80"');
-    }
+    assert(score >= 80, 'The score must be bigger or equal to 80');
+    print('You passed');
   } catch (e) {
-    print('Uncaught Error: Assertion failed: "Please provide a valid integer score."');
+    print('Uncaught Error: Assertion failed: "The score must be bigger or equal to 80"');
   }
 }
