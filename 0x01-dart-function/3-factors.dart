995187ce9be7f0725e55a81e55ae05e5e096120b
mxnc <thibaultmaxence44@gmail.com>
2024-07-04 08:55:27 +0200

    making it recursive

--- 0x01-dart-function/3-factors.dart
+++ 0x01-dart-function/3-factors.dart
@@ -2,24 +2,5 @@ int fact(int f) {
   if (f == 1 || f <= 1) {
     return 1;
   }
-  else {
-    return factorial(f);
-  }
-}
-
-int factorial(int k) {
-  // TODO: Faster algorithm
-  if (k < 0) {
-    throw ArgumentError.value(k);
-  }
-  if (k == 0) {
-    return 1;
-  }
-  var result = k;
-  k--;
-  while (k > 1) {
-    result *= k;
-    k--;
-  }
-  return result;
+  return f * fact(f - 1);
 }
