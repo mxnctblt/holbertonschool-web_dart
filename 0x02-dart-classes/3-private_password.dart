2c669958f1ba99af9963bb6b363e99824f80c242
mxnc <thibaultmaxence44@gmail.com>
2024-07-23 09:23:38 +0200

    task 3 private password

---
+++ 0x02-dart-classes/3-private_password.dart
@@ -0,0 +1,15 @@
+class Password {
+  String _password;
+
+  Password({required password}) : _password = password;
+
+  bool isValid(){
+    RegExp regexp = RegExp(r'^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{8,16}$');
+    return regexp.hasMatch(_password);
+  }
+
+  @override
+  String toString(){
+    return 'Your Password is: $_password';
+  }
+}
