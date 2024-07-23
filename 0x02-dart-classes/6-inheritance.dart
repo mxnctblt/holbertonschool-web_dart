20bbc0bb71f4b367b32f83dabd19878f76df0b55
mxnc <thibaultmaxence44@gmail.com>
2024-07-23 09:46:11 +0200

    fix task 6

--- 0x02-dart-classes/6-inheritance.dart
+++ 0x02-dart-classes/6-inheritance.dart
@@ -7,32 +7,18 @@ class User extends Password {
   int id;
   String user_password;
 
-  User({
-    required this.nq;e,
-    required this.name,
-    required this.age,
-    required this.height,
-    required String user_password,
-  }) : super(password: user_password) {
-    this.user_password = user_password;
+  User({required this.name, required this.age, required this.height, required this.id, required this.user_password}) : super(password: user_password);
+
+  static User fromJson(Map<dynamic, dynamic> userJson){
+    return User(name: userJson['name'], id: userJson['id'], height: userJson['height'], age: userJson['age'], user_password: userJson['user_password']);
   }
 
-  static User fromJson(Map<dynamic, dynamic> userJson) {
-    return User(
-      id: userJson['id'],
-      name: userJson['name'],
-      age: userJson['age'],
-      height: userJson['height'],
-      user_password: userJson['user_password'],
-    );
-  }
-
-  Map<String, dynamic> toJson() {
+  Map<String, dynamic> toJson(){
     return {
-      'id': id,
-      'name': name,
-      'age': age,
-      'height': height,
+      'id': this.id,
+      'name': this.name,
+      'age': this.age,
+      'height': this.height
     };
   }
 
